b2d4f765cd69aad6101b3dbb3d07e7764753dbe4
maqiangfei <qiangfei.ma@qq.com>
2024-10-02 15:36:34 +0800

    feat: 编辑员工信息

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -11,11 +11,12 @@ import org.apache.ibatis.annotations.Select;
 public interface EmployeeMapper {
 
     /**
-     * 根据用户名查询员工
+     * 根据用户名查询员工 （查询出密码）
      * @param username
      * @return
      */
-    @Select("select * from employee where username = #{username}")
+    @Select("select id, `name`, username, phone, password, sex, id_number, `status`, create_time, update_time, create_user, update_user " +
+            "from employee where username = #{username}")
     Employee getByUsername(String username);
 
     /**
@@ -23,8 +24,7 @@ public interface EmployeeMapper {
      * @param employee
      */
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
-            "value" +
-            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
+            "value(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser})")
     void insert(Employee employee);
 
     /**
@@ -33,4 +33,20 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    /**
+     * 根据主键动态修改字段
+     * @param employee
+     */
+    void update(Employee employee);
+
+    /**
+     * 根据id查询员工
+     * @param id
+     * @return
+     */
+    @Select("select id, `name`, username, phone, sex, id_number, `status`, create_time, update_time, create_user, update_user " +
+            "from employee " +
+            "where id = #{id}")
+    Employee getById(Long id);
 }
